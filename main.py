18034e1f9efde9c0e55e8d37ce80d937ba98edf1
Larissa Ramos <68797820+larisramos@users.noreply.github.com>
2022-05-03 11:36:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+valor1 = int(input("Insira o 1° valor inteiro: \n"))
+valor2 = int(input("Insira o 2° valor inteiro: \n"))
+if (valor1 == valor2) :
+  print(f"Os valores informados são IGUAIS! Sendo o seu valor respectivamente {valor1}.")
+else:
+  print(f"Os valores {valor1} e {valor2} anteriormente informados são DIFERENTES!")
